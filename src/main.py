322b552e4a2d97e1e469077b310fe97b763e6f94
andrycodestuff <andrybusa@gmail.com>
2020-09-06 20:39:31 +0200

    moved backend code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from os import path
 
-app = Flask(__name__)
+STATIC_FOLDER_PATH = path.join(path.dirname(__file__), '../static')
+app = Flask(__name__, static_folder=STATIC_FOLDER_PATH)
 
 @app.route('/')
 def hello_world():

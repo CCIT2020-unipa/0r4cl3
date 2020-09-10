80a96f4c65c04e2823037bc66b14994cfc2add3e
andrycodestuff <andrybusa@gmail.com>
2020-09-10 17:18:25 +0200

    fixed cluttered PacketHosts UI

--- static/app/components/PacketList/components/PacketHosts/PacketHosts.tsx
+++ static/app/components/PacketList/components/PacketHosts/PacketHosts.tsx
@@ -7,8 +7,8 @@ export const PacketHosts: React.SFC<IProps> = ({ packet }) => (
   <Row>
     <Col span={11}>
       <Row>
-        <Col span={12}>{packet.host_a_ip}</Col>
-        <Col span={12}>
+        <Col span={16}>{packet.host_a_ip}</Col>
+        <Col span={8}>
           <Tag color='blue'>{packet.host_a_port}</Tag>
         </Col>
       </Row>
@@ -16,8 +16,8 @@ export const PacketHosts: React.SFC<IProps> = ({ packet }) => (
     <Col span={2}>⇋</Col>
     <Col span={11}>
       <Row>
-        <Col span={12}>{packet.host_b_ip}</Col>
-        <Col span={12}>
+        <Col span={16}>{packet.host_b_ip}</Col>
+        <Col span={8}>
           <Tag color='blue'>{packet.host_b_port}</Tag>
         </Col>
       </Row>
